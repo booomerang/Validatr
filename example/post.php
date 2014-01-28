4b83d91802a353f14540209698506f8b85febfe1
Alex Duplii <dusanea@gmail.com>
2014-01-28 22:57:35 +0200

    The work started on 2nd version of Validatr

--- example/post.php
+++ example/post.php
@@ -1,4 +1,59 @@
 <?php
+/*
+$rules =
+'
+required
+min
+max
+email
+bool
+numeric
+alpha
+alnum
+alnumWith
+in
+notIn
+equal
+notEqual
+confirm?
+exists?(DB)
+unique?(DB)
+';
+
+
+$rules = array (
+    'name' => 'required|minLenght:4|maxLenght:5|alnum|equal:Petea',
+    'password' => 'required|minLenght:3',
+    'email' => 'required|email',
+);
+
+$messages = array (
+    'name.required'  => 'Поле обязательно для заполнения',
+    'name.minLength' => 'Минимально !:value []%#^$&@#~!@#$%^&*( допустимоооооо :value символов',
+    'name.maxLength' => 'Not equils for :value',
+    'name.equal'     => 'Максимально допустимо :value символов',
+
+    'email.required' => 'Максимально допустимо :value символов',
+    'email.email'    => 'Максимально допустимо :value символов'
+);
+
+$rules = array (
+    'name' => 'required|minLenght[4,8]|maxLenght:6,8|alnum|equal(Petea,Vasea)',
+    'email' => 'required|email',
+);
+
+$messages = array(
+    'name' => array(
+        'required' => 'Поле обязательно для заполнения',
+        'minLength' => 'Минимально !:value []%#^$&@#~!@#$%^&*( допустимоооооо :value символов',
+        'equal' => 'Not equils for :value',
+        'maxLength' => 'Максимально допустимо :value символов'
+    ),
+    'email' => array(
+        'required' => 'Поле обязательно для заполнения',
+        'email' => 'Максимально допустимо :value символов'
+    )
+);
 
 // Set rules for some fields in form
 $rules = array(
@@ -28,8 +83,8 @@ $messages = array(
     'name' => array(
         'required' => 'Поле обязательно для заполнения',
         'minLength' => 'Минимально !:value []%#^$&@#~!@#$%^&*( допустимоооооо :value символов',
-        'equal' => 'Not equils for :value',
-        'maxLength' => 'Максимально допустимо :value символов'
+        'maxLength' => 'Максимально допустимо :value символов',
+        'equal' => 'Not equils for :value'
     ),
     'password' => array(
         'required' => 'Поле обязательно для заполнения',
@@ -49,6 +104,47 @@ $validator->addRule('equal', function($ruleValue, $fieldValue) {
 $result = $validator->validate($_POST, $rules, $messages);
 
 //var_dump($result);
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";*/
+
+
+
+
+
+
+
+
+
+
+
+
+$rules = array (
+    'name' => 'required|min:4|max:5|alnum|equal:Petea',
+    'password' => 'required|minLenght:3',
+    'email' => 'required|email',
+);
+
+// Your error messages on your language
+$messages = array(
+    'name' => array(
+        'required' => 'Поле обязательно для заполнения',
+        'minLength' => 'Минимально !:value []%#^$&@#~!@#$%^&*( допустимоооооо :value символов',
+        'maxLength' => 'Максимально допустимо :value символов',
+        'equal' => 'Not equils for :value'
+    ),
+    'password' => array(
+        'required' => 'Поле обязательно для заполнения',
+        'maxLength' => 'Максимально допустимо :value символов',
+        'equal' => 'Пароль должен равняться этому значению - :value'
+    )
+);
+
+require_once '../library/Validator.php';
+$validator = new Validatr\Validator();
+
+$result = $validator->validate($_POST, $rules, $messages);
 
 echo "<pre>";
 print_r($result);
