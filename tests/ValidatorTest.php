aa9a1d9af6460dc20f2e25ca9a70bc4f55a155cd
booomerang <dusanea@gmail.com>
2016-03-23 16:28:47 +0200

    Add some new tests for class methods.

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -16,15 +16,43 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider falseRequiredValuesProvider
      */
-    public function testIfIsRequired($input)
+    public function testValidateRequired($input)
     {
         $v = new Validator();
-        $this->assertFalse($v->isRequired($input));
+        $this->assertFalse($v->validateRequired($input));
     }
 
-    public function testIfIsMin()
+    public function testValidateMin()
     {
+        $trueData = [
+            ['fdfsfsf', 3],
+            ['2dsh4353', 3],
+            ['5', 3],
+            [5, 3],
+            ['1b245', 3],
+            ['21b', 2],
+            ['7.5', 2.3],
+            ['2.4', '2.4']
+        ];
+
+        $falseData = [
+            ['fdfsfsf', 7.5],
+            ['2dsh4353', '32.41'],
+            ['5', '5.01'],
+            [4, 8],
+            ['', 1],
+            ['21b', '12'],
+            ['2.3', 2.4]
+        ];
+
         $v = new Validator();
-        $this->assertTrue($v->isMin('dsh4353', 3));
+
+        foreach($trueData as $row) {
+            $this->assertTrue($v->validateMin($row[0], $row[1]));
+        }
+
+        foreach($falseData as $row) {
+            $this->assertFalse($v->validateMin($row[0], $row[1]));
+        }
     }
 }
