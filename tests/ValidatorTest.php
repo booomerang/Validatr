59744df668ce8e611d2bd9c2b7d8da5c8b092e27
booomerang <dusanea@gmail.com>
2016-03-26 14:13:23 +0200

    Add new test for validateMax method.

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -15,6 +15,7 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider falseRequiredValuesProvider
+     * @param $input
      */
     public function testValidateRequired($input)
     {
@@ -53,6 +54,44 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 
         foreach($falseData as $row) {
             $this->assertFalse($v->validateMin($row[0], $row[1]));
+        }
+    }
+
+    public function testValidateMax()
+    {
+        $trueData = [
+            ['fdfsfsf', 10],
+            ['2dsh4353', 9.5],
+            ['5', 7],
+            [5, 6],
+            ['', 1],
+            ['1b245', 6],
+            ['21b', 7],
+            ['7.5', 7.55],
+            ['2.4', '2.5'],
+            [2.4, '2.5']
+        ];
+
+        $falseData = [
+            [4, 2],
+            ['fdfsfsf', 3.2],
+            ['2dsh4353', '3.41'],
+            ['5.01', '5.00'],
+            ['vv', 1],
+            ['21b', '2'],
+            ['2.3', 2.1],
+            [2.12, 1.89],
+            [1.999, '1.99']
+        ];
+
+        $v = new Validator();
+
+        foreach($trueData as $row) {
+            $this->assertTrue($v->validateMax($row[0], $row[1]));
+        }
+
+        foreach($falseData as $row) {
+            $this->assertFalse($v->validateMax($row[0], $row[1]));
         }
     }
 }
